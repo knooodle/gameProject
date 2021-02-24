1ccb83bc4b2d39afe414cc4ea3e1fbefe035d21d
lars <lars.haukland@gmail.com>
2021-02-24 09:24:21 +0100

    blø

--- index.php
+++ index.php
@@ -5,8 +5,9 @@
         <meta charset="utf-8">
         <link rel="stylesheet" type="text/css"  href="style.css">
         <script src="code.js" defer></script>
+        <script str="lib/fuctions.js" defer></script>
     </head>
     <body>
-
+    <canvas id="canvas" height="960" width="1280">
     </body>
 </html>

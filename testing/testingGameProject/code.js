bd11518b9e985104daa1d45d46556af9571ff863
lars <lars.haukland@gmail.com>
2021-03-19 09:33:08 +0100

    chunk working

--- testing/testingGameProject/code.js
+++ testing/testingGameProject/code.js
@@ -1,13 +1,35 @@
+let playerBox = {
+    x:100,
+    y:100,
+    width:20,
+    height:20,
+    xSpeed:100,
+    ySpeed:1
+};
+
+playerBox.update = function(){
+    this.x+=this.xSpeed;
+};
+
+playerBox.draw = function(){
+    ctx.fillRect(this.x,this.y,this.width,this.height);
+};
+
+
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 let things = 2;
+let chunk = 0;
+
+let mapWidth = 30;
 
 let mapUrl = "img/maps/test_map2.json"
-let map;
-let chunkWidth = 16;
+let map,newMap;
 let xmlhttp;
 loadFile(mapUrl);
 
 let a = new Image();
 a.onload = thingLoaded;
 a.src = "img/spritesheets/exampleTilesheet.png";
+
+
--- testing/testingGameProject/lib/functions.js
+++ testing/testingGameProject/lib/functions.js
@@ -7,10 +7,8 @@ function thingLoaded(){
 
 function start(){
     console.log("started");
-    //let scale =  (Math.pow(chunkWidth,2)) / ((ctx.canvas.width / chunkWidth) * (ctx.canvas.height / chunkWidth))    ;
-    let scale = 1;
-    //console.log(scale);
-    drawTiles(map.layers[0].data,ctx,a,map.tileheight,map.layers[0].width,scale);
+    newMap = mapArray(map.layers[0].data);
+    setInterval(gameLoop,100);
 }
 
 function loadFile(url){
@@ -27,28 +25,92 @@ function handleFile(){
     }
 }
 
+function gameLoop(){
+    
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    drawTiles(newMap,ctx,a,map.tileheight,mapWidth,chunk);
+    playerBox.update();
+    if(playerBox.x >= canvas.width){
+        chunk++
+        playerBox.x = 0;
+    }
+    if(chunk > 2){
+        chunk = 0;
+    }
+    playerBox.draw();
+}
 
 
-function drawTiles(map,ctx,tilesheet,tileSideLength,width,scale){
-    // Function for drawing tiles that the player sees. Map array is a 1 dimensional arrays that have tile values. 
 
-    let mapWidth = width;
+function drawTiles(map,ctx,tilesheet,tileSideLength,width,chunk){
+    // Function for drawing tiles that the player sees. Map array is a 1 dimensional arrays that have tile values.
     let tileSheetWidth = tilesheet.width / tileSideLength;
     let x = 0;
     let y = 0;
 
-    for(let a = 0; a<map.length; a++){
-        x = (a % mapWidth) * (tileSideLength * scale);
-        y = Math.floor(a / mapWidth) * (tileSideLength * scale);
+    let scale = (canvas.width / width) / tileSideLength;
 
-        if(map[a]!=0 && a < (16*16)){
-            ctx.drawImage(tilesheet,
-                ((map[a]-1) % tileSheetWidth) * tileSideLength,Math.floor((map[a] -1) / tileSheetWidth) * tileSideLength,
-                tileSideLength,tileSideLength,
-                x,y,
-                tileSideLength * scale,tileSideLength * scale);
-            console.log(a);
+    for(let row = 0; row<map[chunk].length; row++){
+        for(let col = 0; col<map[chunk][row].length; col++){
+            if(map[chunk][row][col] != 0){
+                ctx.drawImage(tilesheet,
+                    ((map[chunk][row][col]-1) % tileSheetWidth) * tileSideLength, Math.floor((map[chunk][row][col] -1) / tileSheetWidth) * tileSideLength,
+                    tileSideLength, tileSideLength,
+                    x,y,
+                    tileSideLength * scale, tileSideLength * scale);
+            }
+            x += tileSideLength;
         }
-        
+        x = 0;
+        y += tileSideLength;
+    }
+}
+
+function mapArray(map){
+    let newArray = new Array(100);
+    for(let i = 0; i<newArray.length; i++){
+        newArray[i] = new Array();
+    }
+    let tempArray = [];
+    let row = 0;
+    let col = 0;
+    for(let t = 0; t<map.length; t++){
+        tempArray.push(map[t]);
+        if((t+1) % 30 == 0){
+            newArray[col + (row * 10)].push(tempArray);
+            tempArray = [];
+            col++;
+        }
+        if(col >= 10){
+            col = 0;
+        }
+        if((t+1) % 4800 == 0){
+            row++;
+        }
+    }
+    return newArray;
+}
+
+function changeChunk(playerX, playerY,map,ctx,tilesheet,tileSideLength,width,chunk){
+    chunk++;
+    let x = 0; 
+    let y = 0;
+    let scale = (ctx.canvas.width / width) / tileSideLength;
+    let tileSheetWidth = tilesheet.width / tileSideLength;
+    for(let i = ctx.canvas.width; i>0; i--){
+        x = i;
+        for(let row = 0; row<map[chunk].length; row++){
+            for(let col = 0; col<map[chunk][row].length; col++){
+                if(map[chunk][row][col] != 0){
+                    ctx.drawImage(tilesheet,
+                        ((map[chunk][row][col]-1) % tileSheetWidth) * tileSideLength, Math.floor((map[chunk][row][col] -1) / tileSheetWidth) * tileSideLength,
+                        tileSideLength, tileSideLength,
+                        x,y,
+                        tileSideLength * scale, tileSideLength * scale);
+                }
+                x += tileSideLength;
+            }
+            y += tileSideLength;
+        }
     }
 }
